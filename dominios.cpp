38d91cca3ebdc5e8be93aa380371efd8f36e5e23
AbnerSSoares <abnerssoares@hotmail.com>
2019-09-14 14:25:21 -0300

    inclusão do metodo verificador de cpf

--- dominios.cpp
+++ dominios.cpp
@@ -55,3 +55,53 @@ bool Dominio::areDigits(string valor) {
     }
     return true;
 }
+
+void Cpf::validar(string valor){
+
+    int vcpf[11];
+
+    for(int i = 0; i < 11; i++){
+        vcpf[i] = static_cast<int>(valor[i] - 48); //Convertendo string para valor absoluto segundo tabela ASCII e passando para array de inteiros//
+
+        if(vcpf[i] < 0 || vcpf[i] > 9){ //Validando a entrada de dados
+            throw std::invalid_argument("Cpf Inválido");
+        }
+    }
+
+    int digito1,
+        digito2,
+        temp = 0;
+
+    //Obtendo o primeiro dígito verificador
+
+    for(int i = 0; i < 9; i++)
+        temp += (vcpf[i] * (10 - i));
+
+    temp %= 11;
+
+    if(temp < 2)
+        digito1 = 0;
+    else
+        digito1 = 11 - temp;
+
+    //Obtendo o segundo digito verificador:
+
+    temp = 0;
+    for(int i = 0; i < 10; i++)
+        temp += (vcpf[i] * (11 - i));
+
+    temp %= 11;
+
+    if(temp < 2)
+        digito2 = 0;
+    else
+        digito2 = 11 - temp;
+
+    /* Se os digitos verificadores obtidos forem iguais aos informados pelo usuário,
+       então o CPF é válido */
+
+    if(digito1 == vcpf[9] && digito2 == vcpf[10])
+        return;
+    else
+        throw std::invalid_argument("Cpf Inválido");
+}
