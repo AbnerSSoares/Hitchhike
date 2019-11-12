2e6b1873b0cb53d910389d4d02778e9bb776edad
Kallebe <kallebe87@gmail.com>
2019-11-12 00:07:42 -0300

    tela autenticação

--- telas.cpp
+++ telas.cpp
@@ -6,20 +6,23 @@
 using namespace std;
 
 void TelaAutenticacao::autenticar(Email *email, Senha *senha) {
-    char lblEmail[25] = "Digite seu email: ";
-    char lblSenha[25] = "Digite sua senha: ";
-    char txtEmail[50], txtSenha[20];
+    char *lblEmail = "Digite seu email: ";
+    char *lblSenha = "Digite sua senha: ";
+    char *txtEmailc, *txtSenhac;
     int linha, coluna;
 
     initscr();
     getmaxyx(stdscr, linha, coluna);
     mvprintw(linha/2, (coluna - strlen(lblEmail))/2, "%s", lblEmail);
-    getstr(txtEmail);
+    getstr(txtEmailc);
     mvprintw(linha/2 + 2, (coluna - strlen(lblSenha))/2, "%s", lblSenha);
-    getstr(txtSenha);
+    getstr(txtSenhac);
     clear();
     endwin();
 
-    email->setValor(txtEmail);
-    senha->setValor(txtSenha);
+    string txtEmails(txtEmailc);
+    string txtSenhas(txtSenhac);
+
+    email->setValor(txtEmails);
+    senha->setValor(txtSenhas);
 }
