4c8ad5c930e99901e9d19e019772a1eb213a1f9b
AbnerSSoares <abnerssoares@hotmail.com>
2019-11-13 13:58:19 -0300

    Stubs completas

--- stubs.cpp
+++ stubs.cpp
@@ -35,6 +35,8 @@ bool StubSUsuario::cadastrar(Usuario &usuario, Conta &conta) throw(runtime_error
     }else if (usuario.getNome().getValor() == TRIGGER_ERRO_SISTEMA_CAD){
         throw runtime_error("Erro de sistema");
     }
+
+    return true;
 }
 bool StubSUsuario::excluir(Usuario &usuario) throw(runtime_error){
 
@@ -48,5 +50,77 @@ bool StubSUsuario::excluir(Usuario &usuario) throw(runtime_error){
     }else if (usuario.getEmail().getValor() == TRIGGER_ERRO_SISTEMA_EXC){
         throw runtime_error("Erro de sistema");
     }
+
+    return true;
+}
+bool StubSCarona::cadastrar(Carona &carona, Usuario &usuario)throw(runtime_error){
+    //Apresentar dados recebidos
+
+    cout << endl << "StubSCarona::cadastrar" << endl ;
+    cout << "Nome = " << carona.getCodigo_de_carona().getValor() << endl ;
+
+    if(carona.getCodigo_de_carona().getValor() == TRIGGER_FALHA_CAD){
+        return false;
+    }else if (carona.getCodigo_de_carona().getValor() == TRIGGER_ERRO_SISTEMA_CAD){
+        throw runtime_error("Erro de sistema");
+    }
+
+    return true;
+}
+/*Carona StubSCarona::pesquisar(Carona &carona)throw(runtime_error){
+    //Apresentar dados recebidos
+
+    cout << endl << "StubSCarona::pesquisar" << endl ;
+    cout << "Nome = " << carona.getCidadeOrigem().getValor() << endl ;
+
+    if(carona.getCidadeOrigem().getValor() == TRIGGER_FALHA_PES){
+        return o que?
+    }else if (carona.getCidadeOrigem().getValor() == TRIGGER_ERRO_SISTEMA_PES){
+        throw runtime_error("Erro de sistema");
+    }
+    return o que?
+}*/
+bool StubSCarona::reservar(Carona &carona, Usuario &usuario)throw(runtime_error){
+    //Apresentar dados recebidos
+
+    cout << endl << "StubSCarona::reservar" << endl ;
+    cout << "Nome = " << carona.getCodigo_de_carona().getValor() << endl ;
+
+    if(carona.getCodigo_de_carona().getValor() == TRIGGER_FALHA_RES){
+        return false;
+    }else if (carona.getCodigo_de_carona().getValor() == TRIGGER_ERRO_SISTEMA_RES){
+        throw runtime_error("Erro de sistema");
+    }
+
+    return true;
 }
 
+bool StubSCarona::cancelar(Carona &carona, Usuario &usuario)throw(runtime_error){
+    //Apresentar dados recebidos
+
+    cout << endl << "StubSCarona::cancelar" << endl ;
+    cout << "Nome = " << carona.getCodigo_de_carona().getValor() << endl ;
+
+    if(carona.getCodigo_de_carona().getValor() == TRIGGER_FALHA_CAN){
+        return false;
+    }else if (carona.getCodigo_de_carona().getValor() == TRIGGER_ERRO_SISTEMA_CAN){
+        throw runtime_error("Erro de sistema");
+    }
+
+    return true;
+}
+
+bool StubSCarona::excluir(Codigo_de_carona &codigo) throw(runtime_error){
+    //Apresentar dados recebidos
+
+    cout << endl << "StubSCarona::excluir" << endl ;
+    cout << "Nome = " << codigo.getValor() << endl ;
+
+    if(codigo.getValor() == TRIGGER_FALHA_DEL){
+        return false;
+    }else if (codigo.getValor() == TRIGGER_ERRO_SISTEMA_DEL){
+        throw runtime_error("Erro de sistema");
+    }
+
+    return true;
+}
